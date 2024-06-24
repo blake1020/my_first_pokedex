4760ad24a89c2330d31efd4eb323ffceb2c7d59b
blake1020 <leandreiablakemore@rocketmail.com>
2024-06-24 12:46:42 -0500

    added pokemon database functions

---
+++ index.js
@@ -0,0 +1,25 @@
+const poke_container = document.getElementById("poke-container")
+// console.log(poke_container)
+const pokemon_count = 150;
+
+
+const fetchPokemons = async () => {
+    for(let id=1; id <= pokemon_count; id++){
+        //we are going to call a function that make the api call
+        await getPokemon(id)
+    }
+}
+
+const getPokemon = async (id) => {
+    try {
+        //api call to the pokeapi
+        const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+        const response = await fetch(url);
+        const pokemon = await response.json();
+        console.log(pokemon);
+    } catch (error) {
+    console.log(error);
+    }
+}
+
+fetchPokemons()
